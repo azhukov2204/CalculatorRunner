13c3bcf594eab4f3011fce34dfdac3a130cc85ce
azhukov2204 <andrew284@mail.ri>
2021-04-04 17:48:06 +0500

    calculator launcher app done

--- app/src/main/java/ru/androidlearning/calculatorrunner/MainActivity.java
+++ app/src/main/java/ru/androidlearning/calculatorrunner/MainActivity.java
@@ -2,7 +2,12 @@ package ru.androidlearning.calculatorrunner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initListeners();
     }
+
+    private void initListeners() {
+        findViewById(R.id.runCalculatorOnUriButton).setOnClickListener(v -> {
+            String UriString = "app://calculator.androidlearning.ru";
+            Uri calculatorUri = Uri.parse(UriString);
+            Intent calculatorRunIntent = new Intent(Intent.ACTION_MAIN, calculatorUri);
+            calculatorRunIntent.addCategory(Intent.CATEGORY_APP_CALCULATOR);
+
+            ActivityInfo activityInfo = calculatorRunIntent.resolveActivityInfo(getPackageManager(), calculatorRunIntent.getFlags());
+            if (activityInfo != null) {
+                startActivity(calculatorRunIntent);
+            } else {
+                Toast.makeText(getApplicationContext(), "URI " + UriString + " not found!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
 }
